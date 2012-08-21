f9040641e2a8fad24a8d35f3baa8277760b63830
Bearice Ren <bearice@gmail.com>
2012-08-21 14:43:50 +0800

    build_env.sh

---
+++ build_env.sh
@@ -0,0 +1,8 @@
+export HOST=arm-kindle-linux-gnueabi
+export CC=$HOST-gcc
+export CPP=$HOST-g++
+export LD=$HOST-ld
+export AS=$HOST-as
+
+export PATH=$PATH:/Volumes/XTools/arm-kindle-linux-gnueabi/bin
+
